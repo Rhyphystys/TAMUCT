2e55b152dcc8c7dd6d0cbdc0199f221b429c6409
Rhyphystys <68401178+Rhyphystys@users.noreply.github.com>
2020-07-16 15:53:36 -0500

    Add files via upload

---
+++ Duke_Chapter14/WORKING CHP 14/Duke_Chapter14/js/yoga.js
@@ -0,0 +1,10 @@
+function last_mod_date()
+{
+	document.getElementById("last_date_mod").innerHTML ="This page was last modified on :" +
+document.lastModified;
+}
+
+function yoga_alert()
+{
+	alert("Yin Yoga classes start next month");
+}
